006f0b1f41a789fe46ec1fc862888145b089a8c1
SophiaAGD <sophiabelenkov@gmail.com>
2021-08-11 16:39:34 +0300

    added UserDataEntity

---
+++ src/main/java/com/app/myrunningtracker/entity/UserDataEntity.java
@@ -0,0 +1,62 @@
+package com.app.myrunningtracker.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+import java.sql.Time;
+
+@Entity
+public class UserDataEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id; //primary key, type: integer
+    private int user_id; //type: integer
+    private java.sql.Date date; //type: date
+    private Time time; //type: time
+    private float distance; //type: float
+
+    public UserDataEntity() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+}
